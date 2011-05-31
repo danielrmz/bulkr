cc62c929e0ff8401b6fd3a97bf1606cf3282ef91
Daniel Ramirez <daniel.rmz@gmail.com>
2011-05-31 01:28:24 -0500

    Clippy css adjustments

--- flickr.com.js
+++ flickr.com.js
@@ -12,29 +12,29 @@ if(window.jQuery) {
                                             "<li class='first last'><a id='bulkDownload' class='Butt'>Download Selected</a></li>" +
                                         "</ul>");
 		$("#main").append('<div class="yui3-popover-content-hider" style="left:-999em;"> '+
-			'<div id="bulkTip" class="yui3-widget yui3-popover yui3-widget-positioned yui3-widget-stacked" style=" width: 300px; left: 12660px; top: -1220px; z-index: 0; ">'+
+			'<div id="bulkTip" class="yui3-widget yui3-popover yui3-widget-positioned yui3-widget-stacked" style="display:none; width: 300px; left: 12660px; top: -1220px; z-index: 0; ">'+
 				'<div id="share-menu-v3" class="share-menu-bs yui3-popover-content yui3-widget-stdmod" style="z-index:100;width:300px;"> ' +
 				'<div style="padding:6px 7px; height:17px; background:#F6F6F6;color:#0063DC;font-weight:bold;border-bottom:1px solid #E3E3E3;z-index:1002;">'+
 					'<img src="http://l.yimg.com/g/images/window_close_grey.gif" alt="" style="float:right;position:relative;top:2px;cursor:pointer;" onclick="document.getElementById(\'bulkTip\').style.display=\'none\';"/>'+
 					'Share the set!'+
 				'</div>'+
-				'<div class="share-options-inner share-options-placeholder" style="background:white;">' +
-				'<input type="text" endpoint="'+service_endpoint+'" id="txtBulkZipLink" readonly="readonly" style="width:90%;height:18px;border:1px solid silver;"/> '+ 
-				' <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="14" height="14" id="clippy" > ' +
+				'<div class="share-options-inner share-options-placeholder" style="background:white;height:30px;">' +
+				'<input type="text" endpoint="'+service_endpoint+'" onclick="this.select();" id="txtBulkZipLink" readonly="readonly" style="width:92%;height:18px;border:1px solid silver;"/> '+ 
+				' <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="16" height="16" id="clippy" style="margin-top:5px;"> ' +
 '    <param name="movie" value="'+service_endpoint+'/clippy.swf"/>' +
 '    <param name="allowScriptAccess" value="always" />' +
 '    <param name="quality" value="high" />' +
 '    <param name="scale" value="noscale" />' +
 '    <param name="FlashVars" value="func=_bulk_getLink&label=&feedback=">' +
 '    <param name="bgcolor" value="#FFFFFF">' +
-'    <embed src="'+service_endpoint+'/clippy.swf" FlashVars="func=_bulk_getLink&label=&feedback="  width="14" height="14" name="clippy" quality="high" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" bgcolor="#FFFFFF" ' +
+'    <embed src="'+service_endpoint+'/clippy.swf" scale="noscale" FlashVars="func=_bulk_getLink&label=&feedback=" style="margin-top:5px;position:relative;top:5px;" width="16" height="16" name="clippy" quality="high" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" bgcolor="#FFFFFF" ' +
 '    />' +
  
 				'</div>'+
 				'<div class="clearfix"></div>' +
 				'</div>' +
 				'<span style="left:250px;color:#F6F6F6;" class="yui3-popover-arrow yui3-popover-arrow-t">◣</span>'+
-				'<span class="yui3-popover-arrow-mask yui3-popover-arrow-mask-t" style="margin-left:70px;z-index:0;"></span>' +
+				'<span class="yui3-popover-arrow-mask yui3-popover-arrow-mask-t" style="margin-left:70px;background:#F6F6F6;"></span>' +
 				'</div>' +
 			'</div>');
 		
